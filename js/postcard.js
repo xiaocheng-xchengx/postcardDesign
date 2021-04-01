ef4f7c024a87881cedc90bc46749951bb8c775ec
kat <syzcitrine@gmail.com>
2021-04-01 14:13:24 -0700

    Impelement send email

--- js/postcard.js
+++ js/postcard.js
@@ -3,6 +3,34 @@ let sender = localStorage.getItem("localStorageTwo");
 let receiver = localStorage.getItem("localStorageThree");
 let stored_answers = JSON.parse(localStorage.getItem("localStorageQuestions"));
 
+// Create Base64 Object
+// Source: https://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+// Read query parameters
+const urlParams = new URLSearchParams(window.location.search);
+
+const senderVal = urlParams.get('s');
+//console.log("sender: " + senderVal);
+if (senderVal != null && senderVal != '') {
+	sender = Base64.decode(senderVal);
+}
+
+const receiverVal = urlParams.get('r');
+if (receiverVal != null && receiverVal != '') {
+	receiver = Base64.decode(receiverVal);
+}
+
+const contentVal = urlParams.get('c');
+if (contentVal != null && contentVal != '') {
+	content = Base64.decode(contentVal);
+}
+
+const answersVal = urlParams.get('a');
+if (answersVal != null && answersVal != '') {
+	stored_answers = JSON.parse(Base64.decode(answersVal));
+}
+
 //set the content as the ones that user inputted.
 window.addEventListener('load', (event) => {
   document.getElementsByClassName("content")[0].getElementsByTagName("p")[0].innerHTML = `Hi, ${receiver},`;
@@ -52,12 +80,29 @@ function saveAs(uri, filename) {
 }
 
 // Snapshot
-function takeshot() {
+function takeShot() {
   html2canvas(document.getElementById("card"), {allowTaint: true}).then(function(canvas) 
 	{
 	  document.getElementById("output").appendChild(canvas);
 	  saveAs(canvas.toDataURL(), 'postcard.png');
 	});
+}
+
+// Snapshot
+function sendEmail() {
+  senderV = Base64.encode(sender);
+  receiverV = Base64.encode(receiver);
+  contentV = Base64.encode(content);
+  answersV = Base64.encode(JSON.stringify(stored_answers));
+  queryParams = `?s=${senderV}%26r=${receiverV}%26c=${contentV}%26a=${answersV}`;
+  fullLink = "https://xiaocheng-xchengx.github.io/postcardDesign/postcard.html" + queryParams
+  console.log("queryParams:"+fullLink);
+  
+  subject = `Postcard from ${sender}`
+  body = `Check out the postcard: ${fullLink}`
+  console.log("body:"+body);
+  
+  window.open(`mailto:?subject=${subject}&body=${body}`);
 }
 
 //Postcard theme starts here
